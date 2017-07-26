bc28e0692d800ff2550568ba1fbf47ba28b7a27d
Duncan Fyfe <duncanfyfe@domenlas.com>
2017-07-26 17:27:19 +0100

    Change user name.

--- config/nodes/admin1.example.com/kubernetes/kubernetes.sh
+++ config/nodes/admin1.example.com/kubernetes/kubernetes.sh
@@ -2,8 +2,7 @@
 # (master) node.
 export ETCD_PROFILE='client'
 export KUBE_PROFILE='kubectl'
-export NODE_USER='duncanfyfe'
-export NODE_CA='digitalocean'
+export NODE_USER='theuserofthiscertificate'
 export NODE_BUILD_ROOT="CHANGEME"
 export NODE_HOSTNAME='foobar.example.com'
 export NODE_IP_ADDR='192.168.10.30'
